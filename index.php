fe0f977c430b583f9011bc74401e2dfe3a6139b8
oumniasabri <oumniasabri>
2023-10-17 13:32:41 +0100

    index

--- index.php
+++ index.php
@@ -1,33 +1,3 @@
-<?php
-
-require_once "Personne.class.php";
-require_once "voiture.class.php";
-
-
-// $v1 = new Voiture("hi","za","ihui");
-
-// echo $v1->color;
-// echo "<br>";
-// $v1->stop(20);
-
-
-
-$p1 = new Personne("SABRI","Oumnia",24);
-
-echo $p1->nom." ".$p1->prenom." ".$p1->getAge()." ans";
-echo "<br>";
-echo $p1;
-
-echo "<br>";
-$p1->setAge(25);
-echo $p1->nom." ".$p1->prenom." ".$p1->getAge()." ans";
-
-echo "<br>";
-
-
-// $p1->afficher();
-
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -73,6 +43,42 @@ echo "<br>";
     </div>
   </div>
 </nav>
+<?php
+
+require_once "Personne.class.php";
+require_once "voiture.class.php";
+require_once "Student.class.php";
+
+
+
+// $v1 = new Voiture("hi","za","ihui");
+
+// echo $v1->color;
+// echo "<br>";
+// $v1->stop(20);
+
+
+
+$p1 = new Personne("SABRI","Oumnia",24);
+
+echo $p1->nom." ".$p1->prenom." ".$p1->getAge()." ans";
+echo "<br>";
+echo $p1;
+
+echo "<br>";
+$p1->setAge(25);
+echo $p1->nom." ".$p1->prenom." ".$p1->getAge()." ans";
+
+echo "<br>";
+
+$student1 = new Student("SABRI","Oumnia",24,19);
+echo $student1->nom." ".$student1->prenom." ".$student1->getAge()." ans";
+
+echo $student1;
+
+// $p1->afficher();
+
+?>
 </body>
 </html>
 
