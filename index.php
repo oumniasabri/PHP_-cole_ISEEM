681fecb39042feac097ccd3e550b4f64be740e2d
oumniasabri <oumniasabri>
2023-10-17 11:17:50 +0100

    m

--- index.php
+++ index.php
@@ -15,9 +15,25 @@ require_once "voiture.class.php";
 $p1 = new Personne("SABRI","Oumnia",24);
 
 echo $p1->nom." ".$p1->prenom." ".$p1->getAge()." ans";
+echo "<br>";
+echo $p1;
 
-// echo "<br>";
+echo "<br>";
 $p1->setAge(25);
 echo $p1->nom." ".$p1->prenom." ".$p1->getAge()." ans";
 // $p1->afficher();
+
+
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 
