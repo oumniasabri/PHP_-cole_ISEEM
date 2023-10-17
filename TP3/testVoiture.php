d3f65902ee11e2b706239f7b839bb79ae85bc48b
oumniasabri <oumniasabri>
2023-10-17 14:19:28 +0100

    test

---
+++ TP3/testVoiture.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="">
+        <table border="1">
+            <tr>
+                <td colspan="2">Veuillez remplir les infos suivants:</td>
+            </tr>
+            <tr>
+                <td>Matricule</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Marque</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Modèle</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Annee</td>
+                <td>
+                <select name="" id="">
+                    <option value="2010">2010</option>
+                    <option value="2011">2011</option>
+                    <option value="2012">2012</option>
+                    <option value="2013">2013</option>
+                    <option value="2014">2014</option>
+                </select></td>
+            </tr>
+            <tr>
+                <td><input type="button" value="Valider"></td>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
