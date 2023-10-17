48634e6691a591dbff170a16d0919495fcff8d70
oumniasabri <oumniasabri>
2023-10-17 09:47:52 +0100

    modif

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ require_once "voiture.class.php";
 $v1 = new Voiture();
 
 echo $v1->color;
+
+$v1->stop(20);
--- voiture.class.php
+++ voiture.class.php
@@ -4,4 +4,9 @@ class Voiture{
     private $marque = "Dacia";
     public $color = "Blue";
     protected $model;
+
+    public function stop($speed)
+    {
+        echo $this->marque.' speed ='.$vitesse;
+    }
 }
